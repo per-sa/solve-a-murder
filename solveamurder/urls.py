d5e94d6baa751c6a817da4a34d3c91eb1a0b5f70
matheusclmb <ccolombomatheus@gmail.com>
2022-09-23 23:33:54 +0100

    started working on index page

--- solveamurder/urls.py
+++ solveamurder/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+
+    # auth
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
