b3defc52b8d9800bb43752ffa7655ff0d089235e
Matheus Colombo <clmb@Matheuss-MBP.lan>
2022-09-27 15:14:14 +0100

    working on url routes/pk for details of suspects e etc

--- solveamurder/urls.py
+++ solveamurder/urls.py
@@ -9,7 +9,14 @@ from .views import *
 urlpatterns = [
     # pages
     path('', home, name='home'),
+
     path('victims/', victims, name='victims'),
+    path('victims/<int:pk>/', victim_info, name='victim_info'),
+
+    path('murders/', murders, name='murders'),
+    path('murders/<int:pk>/', murder_info, name='murder_info'),
+
+    path('suspects/<int:pk>', suspects_info, name='suspect_info'),
 
     # auth
     path('login/', auth_views.LoginView.as_view(template_name="pages/login.html"), name='login'),
--- solveamurder/views.py
+++ solveamurder/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from multiprocessing import context
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .models import *
@@ -12,10 +13,12 @@ def home(request):
     total_suspects = Suspect.objects.all().count()
     total_victims = Victim.objects.all().count()
     total_unsolved = Cases.objects.filter(is_unsolved=True).count()
+    total_users = User.objects.all().count()
     context = {
         'total_suspects': total_suspects,
         'total_victims': total_victims,
         'total_unsolved': total_unsolved,
+        'total_users': total_users,
     }
 
     return render(request, 'pages/index.html', context)
@@ -27,6 +30,41 @@ def victims(request):
     }
 
     return render(request, 'pages/victims.html', context)
+
+def victim_info(request, pk):
+    victim = get_object_or_404(Victim, pk=pk)
+
+    context = {
+        'victim': victim,
+    }
+
+    return render(request, 'pages/victiminfo.html', context)
+
+def murders(request):
+    cases = Cases.objects.all()
+    context = {
+        'cases': cases,
+    }
+
+    return render(request, 'pages/murders.html', context)
+
+def murder_info(request, pk):
+    murder = get_object_or_404(Cases, pk=pk)
+
+    context = {
+        'murder': murder,
+    }
+
+    return render(request, 'pages/murderinfo.html', context)
+
+def suspects_info(request, pk):
+    suspect = get_object_or_404(Suspect, pk=pk)
+
+    context = {
+        'suspect': suspect, 
+    }
+
+    return render(request, 'pages/suspectinfo.html', context)
 
 @login_required(login_url=login_url)
 def editprofile(request):
